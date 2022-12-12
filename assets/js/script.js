518d6237f270c5fc48e5c3483b9ce902b97000c8
LionHeart <jerrybeau89@gmail.com>
2022-12-12 13:04:08 -0800

    Changed HTML and JS files to add hours and started working on styling for hours

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,12 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
-$(function () {
+
+let weekDay = dayjs().format('dddd MMM Do');
+let allHourStyles = $('[hour-style]');
+let currentHourStyle;
+
+//$(function () {
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
@@ -14,10 +19,34 @@ $(function () {
   // attribute of each time-block be used to conditionally add or remove the
   // past, present, and future classes? How can Day.js be used to get the
   // current hour in 24-hour time?
+
+  function hourStyle() {
+    for (let i = 0; i < allHourStyles.length; i++) {
+      currentHourStyle = parseInt(allHourStyles[i].dataset.hour);
+      if (currentHourStyle < dayjs.format("HH")) {
+        allHourStyles[i].classList.add('past');
+      } else if (currentHourStyle > dayjs.format("HH")) {
+        allHourStyles[i].classList.add("future");
+      } else {
+        allHourStyles[i].classList.add("present");
+      }
+    }
+  }
   //
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
   //
   // TODO: Add code to display the current date in the header of the page.
-});
+
+  function setClock () {
+    $('#clock').html(dayjs().format('hh:mm:ss a'));
+  }
+
+  $('#currentDay').text(weekDay);
+  setClock();
+  setInterval(setClock, 1000);
+
+  hourStyle();
+  setInterval(hourStyle, 1000)
+//});
